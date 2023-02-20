98f92e89e776f487d15ac162bd72342e1b929edf
zomboy-22 <80216718+zomboy-22@users.noreply.github.com>
2023-02-20 22:31:11 +0530

    Create hello_2.cpp

---
+++ hello_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "New File\n";
+  cout << "Pipeline Execution\n";
+  cout << "PES1UG20CS354\n";
+  cout << "Successfully Executed a Pipeline\n";
+  return 0;
+}
